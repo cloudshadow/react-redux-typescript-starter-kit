bfdbb02617c59d527beeb87686a12bdcdbaa5701
Cloud He <jingye.he@iherb.com>
2020-09-09 10:02:29 +0800

    update hook for  react-redux

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,40 +1,21 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import rootAction from '@/actions';
 import HomeComponent from '@/components/Home/HomeComponent';
-import { RootAction, RootState } from '@/types/GlobalTypes';
+import { RootState } from '@/types/GlobalTypes';
 import { IHomeState } from '@/types/HomeTypes';
 
-interface IHomeProps {
-  fetchTitleEpicRequest: () => void;
-  fetchTitleThunk: () => void;
-  homeState: IHomeState;
-}
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const homeState = useSelector<RootState, IHomeState>((state) => state.home, shallowEqual);
 
-const HomePage = (props: IHomeProps) => {
   return (
     <HomeComponent
-      fetchTitleThunk={props.fetchTitleThunk}
-      fetchTitleEpicRequest={props.fetchTitleEpicRequest}
-      homeState={props.homeState}
+      fetchTitleThunk={(args) => dispatch(rootAction.homeActions.fetchTitleThunk(args))}
+      fetchTitleEpicRequest={() => dispatch(rootAction.homeActions.fetchTitleEpicAsync.request())}
+      homeState={homeState}
     />
   );
 };
 
-const mapStateToProps = (state: RootState) => {
-  return {
-    homeState: state.home,
-  };
-};
-
-interface IMapDispatchToProps {
-  fetchTitleThunk: (args: IHomeState) => void;
-  fetchTitleEpicRequest: () => void;
-}
-const mapDispatchToProps = (dispatch: Dispatch<RootAction>): IMapDispatchToProps => ({
-  fetchTitleThunk: (args) => dispatch(rootAction.homeActions.fetchTitleThunk(args)),
-  fetchTitleEpicRequest: () => dispatch(rootAction.homeActions.fetchTitleEpicAsync.request()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default HomePage;
